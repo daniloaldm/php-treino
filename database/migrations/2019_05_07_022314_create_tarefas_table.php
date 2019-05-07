8c66d0094d901db8e3d0116fb84c7d476a8dab1a
daniloaldm <danilo.alexandrinodm@gmail.com>
2019-05-06 23:34:56 -0300

    Create migrations

---
+++ database/migrations/2019_05_07_022314_create_tarefas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTarefasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tarefas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome', 50);
+            $table->text('descricao');
+            $table->date('prazo');
+            $table->string('prioridade', 50);
+            $table->string('situacao', 50);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tarefas');
+    }
+}
